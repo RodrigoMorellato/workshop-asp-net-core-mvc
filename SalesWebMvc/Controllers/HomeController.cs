48398f28794d3e4d3cfb8088ec7a1646589b3ea2
Rodrigo Morellato <rodrigo.morellato@gmail.com>
2019-11-28 20:07:07 -0300

    Files program

---
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models.ViewModels;
+using System.Diagnostics;
+
+namespace SalesWebMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Salles Web MVC App from C#";
+            ViewData["Desenvolvedor"] = "Rodrigo Chaves Morellato";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
